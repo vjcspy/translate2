1e30970f064ee608db1604ad0508f87912733bf0
wsl <mr.vjcspy@gmail.com>
2024-02-25 22:11:01 +0700

    feat: validate

--- api/translate-r.ts
+++ api/translate-r.ts
@@ -3,6 +3,9 @@ import {BASE_URL} from "../common/config";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const data = req.body
+    if(typeof data !== 'object' || !data.hasOwnProperty('key')){
+        return res.status(400)
+    }
     const key = data.key
     const getRes = await fetch(`${BASE_URL}/flag/load?key=${key}`);
 
--- api/translate-s.ts
+++ api/translate-s.ts
@@ -4,6 +4,10 @@ import {BASE_URL} from "../common/config";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const data = req.body
 
+    if (typeof data !== 'object' || !data.hasOwnProperty('key') || !data.hasOwnProperty('value')){
+        return res.status(400);
+    }
+
     const customHeaders = {
         "Content-Type": "application/json",
     }
@@ -11,7 +15,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const saveRes = await fetch(`${BASE_URL}/flag/save`, {
         method: "POST",
         headers: customHeaders,
-        body: JSON.stringify(data),
+        body: JSON.stringify({
+            key: data.key,
+            value: data.value
+        }),
     });
 
     // console.log(saveRes)
